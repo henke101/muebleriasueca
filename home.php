a9a3e71bbf21a077f3dff7105b555f908e0fb524
Henrik Andersson <heenrik@hotmail.com>
2016-02-22 14:37:02 -0300

    add home skeleton

--- home.php
+++ home.php
@@ -1,1 +1,66 @@
-<h1>Home is where the hatred is</h1>
+<section class="startpage_floor">
+	<img src="floor_calc_bg.png" alt="bg_image">
+	<h1 class="startpage_floor_name">Hora de cambiar suelos?</h1>
+	<p class="startpage_floor_descr"> Our floors are much more cheap than in the other stores and are fucking amazing and all that. You’d be a retard not to buy them! Starting from only 99.000 Gs/m<sup>2</sup>. Use the calculator to see your price!
+	</p>
+	<a href="?page=floors">View the floor special</a>
+	<form>
+  		<input type="number" placeholder="Calcule el precio aquí, p. ej. 25 m2"  min="0"><br>
+  		<input type="submit" value="Calcular">
+	</form>
+	<p class="startpage_precio_total"> Tu precio total sería <br> 1.485.000 Gs
+	</p>
+</section>
+<div>
+	<img src="carousel_bg.png" alt="bg_image">
+	<img src="carousel_bg2.png" alt="bg_image">
+	<p>CAMA Henrik <br> 2.000.000 Gs</p>
+	<a href="">Next</a>
+	<a href="">Previous</a>
+</div>
+<div>
+	<article>
+		<img src="http://placehold.it/300x400">
+		<h1>MALM</h1>
+		<p>Cama</p>
+		<p>3.000.000 Gs</p>
+		<p><s>3.500.000 Gs</s></p>
+	</article>
+	<article>
+		<img src="http://placehold.it/300x400">
+		<h1>MALM</h1>
+		<p>Cama</p>
+		<p>3.000.000 Gs</p>
+		<p><s>3.500.000 Gs</s></p>
+	</article>
+	<article>
+		<img src="http://placehold.it/300x400">
+		<h1>MALM</h1>
+		<p>Cama</p>
+		<p>3.000.000 Gs</p>
+		<p><s>3.500.000 Gs</s></p>
+	</article>
+	<article>
+		<img src="http://placehold.it/300x400">
+		<h1>MALM</h1>
+		<p>Cama</p>
+		<p>3.000.000 Gs</p>
+		<p><s>3.500.000 Gs</s></p>
+	</article>
+</div>
+<div>
+	<p>
+		“That Muebleria Sueca shit, it was aight, it was aighty ouknowI'msayin, that shit was aight, it was cool. But my shit is J--more John Blaze than that! I got John Blaze shit” <br>
+		- The Madd Rapper, N.Y, USA
+	</p>
+	<p>
+		“Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut posuere consectetur massa. Vivamus accumsan convallis eleifend. Praesent imperdiet placerat nibh et posuere.” <br>
+		- Latin guy, Rome, Italy
+	</p>
+	<ul>
+		<li><a href="">Testimonio1</a></li>
+		<li><a href="">Testimonio2</a></li>
+		<li><a href="">Testimonio3</a></li>
+		<li><a href="">Testimonio4</a></li>
+	</ul>
+</div>
