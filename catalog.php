83f5db82ee1e5a875d56d35b1c933a433581bfb1
Henrik Andersson <heenrik@hotmail.com>
2016-02-22 14:36:51 -0300

    add catalog skeleton

--- catalog.php
+++ catalog.php
@@ -1,1 +1,98 @@
-<h1>Catálogo</h1>
+<ul>
+    <li><a href="?catID=0">Productos populares</a></li>
+    <li><a href="?catID=1">Sillas</a></ul></li>
+    <li><a href="?catID=2">Mesas</a></li>
+    <li><a href="?catID=3">Estanterías</a></li>
+    <li><a href="?catID=4">Escritórios</a></li>
+    <li><a href="?catID=5">Lámparas</a></li>
+    <li><a href="?catID=6">Suelos</a></li>
+    <li><a href="?catID=7">Baño</a></li>
+    <li><a href="?catID=8">Niños</a></li>
+    <li><a href="?catID=9">Otros</a></li>
+</ul>
+<article>
+	<img src="http://placehold.it/300x400">
+	<h1>MALM</h1>
+	<p>Cama</p>
+	<p>3.000.000 Gs</p>
+	<p><s>3.500.000 Gs</s></p>
+</article>
+<article>
+	<img src="http://placehold.it/300x400">
+	<h1>MALM</h1>
+	<p>Cama</p>
+	<p>3.000.000 Gs</p>
+	<p><s>3.500.000 Gs</s></p>
+</article>
+<article>
+	<img src="http://placehold.it/300x400">
+	<h1>MALM</h1>
+	<p>Cama</p>
+	<p>3.000.000 Gs</p>
+	<p><s>3.500.000 Gs</s></p>
+</article>
+<article>
+	<img src="http://placehold.it/300x400">
+	<h1>MALM</h1>
+	<p>Cama</p>
+	<p>3.000.000 Gs</p>
+	<p><s>3.500.000 Gs</s></p>
+</article>
+<br>
+<article>
+	<img src="http://placehold.it/300x400">
+	<h1>MALM</h1>
+	<p>Cama</p>
+	<p>3.000.000 Gs</p>
+	<p><s>3.500.000 Gs</s></p>
+</article>
+<article>
+	<img src="http://placehold.it/300x400">
+	<h1>MALM</h1>
+	<p>Cama</p>
+	<p>3.000.000 Gs</p>
+	<p><s>3.500.000 Gs</s></p>
+</article>
+<article>
+	<img src="http://placehold.it/300x400">
+	<h1>MALM</h1>
+	<p>Cama</p>
+	<p>3.000.000 Gs</p>
+	<p><s>3.500.000 Gs</s></p>
+</article>
+<article>
+	<img src="http://placehold.it/300x400">
+	<h1>MALM</h1>
+	<p>Cama</p>
+	<p>3.000.000 Gs</p>
+	<p><s>3.500.000 Gs</s></p>
+</article>
+<br>
+<article>
+	<img src="http://placehold.it/300x400">
+	<h1>MALM</h1>
+	<p>Cama</p>
+	<p>3.000.000 Gs</p>
+	<p><s>3.500.000 Gs</s></p>
+</article>
+<article>
+	<img src="http://placehold.it/300x400">
+	<h1>MALM</h1>
+	<p>Cama</p>
+	<p>3.000.000 Gs</p>
+	<p><s>3.500.000 Gs</s></p>
+</article>
+<article>
+	<img src="http://placehold.it/300x400">
+	<h1>MALM</h1>
+	<p>Cama</p>
+	<p>3.000.000 Gs</p>
+	<p><s>3.500.000 Gs</s></p>
+</article>
+<article>
+	<img src="http://placehold.it/300x400">
+	<h1>MALM</h1>
+	<p>Cama</p>
+	<p>3.000.000 Gs</p>
+	<p><s>3.500.000 Gs</s></p>
+</article>
