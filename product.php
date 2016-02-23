5f2beadf91143ca6cb5f04a9535a06ad379ac6a0
Henrik Andersson <heenrik@hotmail.com>
2016-02-23 19:05:45 -0300

    add product page skeleton

---
+++ product.php
@@ -0,0 +1,65 @@
+<nav>
+	<ol>
+		<li><a href="?page=catalog">CATÁLOGO</a></li>
+		<li><p> / </p></li>
+		<li><a href="?page=catalog&id=1">MESAS</a></li>
+		<li><p> / </p></li>
+		<li><p>LACK</p></li>
+	</ol>
+</nav>
+<section>
+	<img src="prod_lack.png" alt="Mesa Lack">
+	<div>
+		<h1>LACK</h1>
+		<p class="prod_type">Mesa auxilar</p>
+		<p class="prod_price">100.000 Gs</p>
+		<p class="prod_descr">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut posuere consectetur massa.</p>
+		<p>
+			<ul>
+				<li><p>Longitud: 55 cm</p></li>
+				<li><p>Ancho: 55 cm</p></li>
+				<li><p>Altura: 45 cm</p></li>
+			</ul>
+		</p>
+		<p>Unidades disponibles en la tienda:</p>
+		<p>0</p>
+		<p>Unidades disponibles en el deposito:</p>
+		<p>5</p>
+	</div>
+	
+</section>
+<section class="see_also">
+	<p>Véase también</p>
+	<a class="arrow arrow-prev" href="">Prev</a>
+	<a class="arrow arrow-next" href="">Next</a>
+	<article>
+		<img src="http://placehold.it/240x340">
+		<h1>MALM</h1>
+		<p>Cama</p>
+		<p>3.000.000 Gs</p>
+		<p><s>3.500.000 Gs</s></p>
+	</article>
+	<article>
+		<img src="http://placehold.it/240x340">
+		<h1>MALM</h1>
+		<p>Cama</p>
+		<p>3.000.000 Gs</p>
+		<p><s>3.500.000 Gs</s></p>
+	</article>
+	<article>
+		<img src="http://placehold.it/240x340">
+		<h1>MALM</h1>
+		<p>Cama</p>
+		<p>3.000.000 Gs</p>
+		<p><s>3.500.000 Gs</s></p>
+	</article>
+	<article>
+		<img src="http://placehold.it/240x340">
+		<h1>MALM</h1>
+		<p>Cama</p>
+		<p>3.000.000 Gs</p>
+		<p><s>3.500.000 Gs</s></p>
+	</article>
+
+</section>
+
