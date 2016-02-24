2a35c6d55abf2faec1bc2f27cb7c8d66b9dd6003
Henrik Andersson <heenrik@hotmail.com>
2016-02-24 17:42:35 -0300

    add white bg to solutions page

--- solutions.php
+++ solutions.php
@@ -1,25 +1,27 @@
-<h1 class="intro_title">Soluciones completos</h1>
-<p class="intro_text">
-	Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut posuere consectetur massa. Vivamus accumsan convallis eleifend. Praesent imperdiet placerat nibh et posuere. Nam eget augue ac urna semper auctor sit amet ac tortor. Integer nisl velit, luctus ut felis eget, ornare rutrum libero. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut posuere consectetur massa.
-</p>
-<div>
-	<h2 class="project_type">Sala</h2>
-	<p>
-	Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut posuere consectetur massa. Vivamus accumsan convallis eleifend. Praesent imperdiet placerat nibh et posuere. Nam eget augue ac urna semper auctor sit amet ac tortor. Integer nisl velit, luctus ut felis eget, ornare rutrum libero. 
+<div class="white_bg">
+	<h1 class="intro_title">Soluciones completos</h1>
+	<p class="intro_text">
+		Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut posuere consectetur massa. Vivamus accumsan convallis eleifend. Praesent imperdiet placerat nibh et posuere. Nam eget augue ac urna semper auctor sit amet ac tortor. Integer nisl velit, luctus ut felis eget, ornare rutrum libero. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut posuere consectetur massa.
 	</p>
-	<img src="solucion_sala.jpg" alt="Solucion sala" >
-</div>
-<div>
-	<h2 class="project_type">Comedor</h2>
-	<img src="solucion_comedor.jpg" alt="Solucion comedor" >
-	<p>
-	Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut posuere consectetur massa. Vivamus accumsan convallis eleifend. Praesent imperdiet placerat nibh et posuere. Nam eget augue ac urna semper auctor sit amet ac tortor. Integer nisl velit, luctus ut felis eget, ornare rutrum libero.
-	</p>
-</div>
-<div>
-	<h2 class="project_type">Oficina</h2>
-	<p>
-	Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut posuere consectetur massa. Vivamus accumsan convallis eleifend. Praesent imperdiet placerat nibh et posuere. Nam eget augue ac urna semper auctor sit amet ac tortor. Integer nisl velit, luctus ut felis eget, ornare rutrum libero. 
-	</p>
-	<img src="solucion_oficina.jpg" alt="Solucion oficina" >
+	<div>
+		<h2 class="project_type">Sala</h2>
+		<p>
+			Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut posuere consectetur massa. Vivamus accumsan convallis eleifend. Praesent imperdiet placerat nibh et posuere. Nam eget augue ac urna semper auctor sit amet ac tortor. Integer nisl velit, luctus ut felis eget, ornare rutrum libero. 
+		</p>
+		<img src="solucion_sala.jpg" alt="Solucion sala" >
+	</div>
+	<div>
+		<h2 class="project_type">Comedor</h2>
+		<img src="solucion_comedor.jpg" alt="Solucion comedor" >
+		<p>
+			Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut posuere consectetur massa. Vivamus accumsan convallis eleifend. Praesent imperdiet placerat nibh et posuere. Nam eget augue ac urna semper auctor sit amet ac tortor. Integer nisl velit, luctus ut felis eget, ornare rutrum libero.
+		</p>
+	</div>
+	<div>
+		<h2 class="project_type">Oficina</h2>
+		<p>
+			Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut posuere consectetur massa. Vivamus accumsan convallis eleifend. Praesent imperdiet placerat nibh et posuere. Nam eget augue ac urna semper auctor sit amet ac tortor. Integer nisl velit, luctus ut felis eget, ornare rutrum libero. 
+		</p>
+		<img src="solucion_oficina.jpg" alt="Solucion oficina" >
+	</div>
 </div>
