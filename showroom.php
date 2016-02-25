43d69640f54a2e19f5d939ccfe02dc91456436a0
Henrik Andersson <heenrik@hotmail.com>
2016-02-25 20:56:55 -0300

    add style to showroom page

--- showroom.php
+++ showroom.php
@@ -1,15 +1,29 @@
-<h1 class="intro_title">Nuestro Showroom</h1>
-<div>
-	<p>
-	Nuestro Showroom está abierto los lunes a viernes 09.00 - 19.00 y los sábados 09.00 - 16.00. Aqui puedes encontrar todo lo que buscas de productos de IKEA entre otros.  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut posuere consectetur massa.
-	</p>
-	<img src="showroom_table.jpg" alt="Mesa showroom" >
+<div class="bg_container">
+	<h1 class="intro_title">Nuestro Showroom</h1>
+
+	<div class="project_segment cf">
+		<div class="project_text_area">
+			<p class="showroom_text">
+				Nuestro Showroom está abierto los lunes a viernes 09.00 - 19.00 y los sábados 09.00 - 16.00. Aqui puedes encontrar todo lo que buscas de productos de IKEA entre otros.  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut posuere consectetur massa. 
+			</p>
+		</div>
+		<div class="project_foto_area">
+			<img src="showroom_table.jpg" alt="Mesa showroom" >
+		</div>
+	</div>
+	<div class="project_segment cf">
+		<div class="project_foto_area opposite_segment">
+			<img src="showroom_kids.jpg" alt="Niños showroom" >
+		</div>
+		<div class="project_text_area opposite_segment">
+			<p class="showroom_text">
+				Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut posuere consectetur massa. Vivamus accumsan convallis eleifend. Praesent imperdiet placerat nibh et posuere. Nam eget augue ac urna semper auctor sit amet ac tortor. Integer nisl velit, luctus ut felis eget, ornare rutrum libero.
+			</p>
+		</div>
+	</div>
+	<div class="find_us">
+		<h1 class="intro_title find_us_title">Encuéntranos</h1>
+		<img src="maps.png" alt="Google maps" >
+	</div>
 </div>
-<div>
-	<img src="showroom_kids.jpg" alt="Niños showroom" >
-	<p>
-	Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut posuere consectetur massa. Vivamus accumsan convallis eleifend. Praesent imperdiet placerat nibh et posuere. Nam eget augue ac urna semper auctor sit amet ac tortor. Integer nisl velit, luctus ut felis eget, ornare rutrum libero.
-	</p>
-</div>
-<h1>Encuéntranos</h1>
-<img src="maps.png" alt="Google maps" >
+
