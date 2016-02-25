c13855b0907c1f0f390009b7f4d770269a8178e0
Henrik Andersson <heenrik@hotmail.com>
2016-02-25 20:23:49 -0300

    add style to before/after section in floor page

--- floors.php
+++ floors.php
@@ -29,16 +29,53 @@
 			</div>
 		</div>
 		<div class="project_foto_area">
-			<img src="before_slatten.png" alt="Antes Slatten">
+			<!-- <img src="before_slatten.png" alt="Antes Slatten"> -->
 			<img src="after_slatten.png" alt="Después Slatten">
-			<br>
-			<a href="">Antes</a>
-			<p>|</p>
-			<a href="">Después</a>
+			<div class="before_after_links">
+			<a class="before_button" href="#">Antes</a>
+				<p>|</p>
+				<a class="after_button" href="#">Después</a>
+			</div>
 		</div>
 	</div>
 
 	<div class="project_segment cf">
+		<div class="project_text_area floor_info opposite_segment">
+			<div class="floor_info_top cf">
+				<div class="floor_text_name_det">
+					<h2 class="floor_name">Tundra</h2>
+					<ul class="floor_details">
+						<li>Suelo Laminado</li>
+						<li>Ancho: 19 cm</li>
+						<li>Longitud: 130 cm</li>
+						<li>Peso: 15 kg</li>
+					</ul>
+				</div>
+				<div class="floor_text_price_pic">
+					<p class="floor_price_m2">129.000 Gs/m<sup>2</sup></p>
+					<img src="floor_tundra.png" alt="Suelo tundra">
+				</div>
+			</div>
+			<div class="floor_text_calc">
+				<form>Calcule el precio total aquí<br>
+					<input class="floor_input_spinner" type="number" placeholder="Por ejemplo 25 m2"  min="0"><br>
+					<input class="floor_input_submit" type="submit" value="Calcular">
+				</form>
+				<p class="floor_precio_total"> Tu precio total sería <br> 882.985.000 Gs</p>
+			</div>
+		</div>
+		<div class="project_foto_area">
+			<!-- <img src="before_slatten.png" alt="Antes Slatten"> -->
+			<img src="after_tundra.png" alt="Después Tundra">
+			<div class="before_after_links">
+			<a class="before_button" href="#">Antes</a>
+				<p>|</p>
+				<a class="after_button" href="#">Después</a>
+			</div>
+		</div>
+	</div>
+
+	<!--<div class="project_segment cf">
 		<div class="project_foto_area opposite_segment">
 			<img src="before_tundra.png" alt="Antes Tundra">
 			<img src="after_tundra.png" alt="Después Tundra">
@@ -64,4 +101,4 @@
 			<p class="precio_total"> Tu precio total sería <br> 1.485.000 Gs</p>
 		</div>
 	</div>
-</div>
+</div> -->
