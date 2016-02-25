e82afccbe694d5ff684fdfc21886ca158804114f
Henrik Andersson <heenrik@hotmail.com>
2016-02-25 19:44:01 -0300

    add style to price calculator in floor page

--- floors.php
+++ floors.php
@@ -1,59 +1,67 @@
-<h1 class="intro_title">Especial de suelos</h1>
-<p class="intro_text">
-	Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut posuere consectetur massa. Vivamus accumsan convallis eleifend. Praesent imperdiet placerat nibh et posuere. Nam eget augue ac urna semper auctor sit amet ac tortor. Integer nisl velit, luctus ut felis eget, ornare rutrum libero. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut posuere consectetur massa. 
-</p>
-<div class="floor_special">
-	<div class="floor_calc">
-		<h2 class="floor_name">Slätten</h2>
-		<p class="floor_price_m2"> 99.000 Gs/m<sup>2</sup> </p>
-		<ul>
-			<li>Suelo Laminado</li>
-			<li>Ancho: 19 cm</li>
-			<li>Longitud: 130 cm</li>
-			<li>Peso: 15 kg</li>
-		</ul>
-		<img src="floor_slatten.png" alt="Suelo slatten">
-		<form>
-	  		Calcule el precio aquí<br>
-	  		<input type="number" placeholder="p. ej. 25 metros cuadrados"  min="0"><br>
-	  		<input type="submit" value="Calcular">
-		</form>
-		<p class="precio_total"> Tu precio total sería <br> 1.485.000 Gs</p>
+<div class="bg_container">
+	<h1 class="intro_title">Especial de suelos</h1>
+	<p class="intro_text">
+		Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut posuere consectetur massa. Vivamus accumsan convallis eleifend. Praesent imperdiet placerat nibh et posuere. Nam eget augue ac urna semper auctor sit amet ac tortor. Integer nisl velit, luctus ut felis eget, ornare rutrum libero. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut posuere consectetur massa. 
+	</p>
+	<div class="project_segment cf">
+		<div class="project_text_area floor_info">
+			<div class="floor_info_top cf">
+				<div class="floor_text_name_det">
+					<h2 class="floor_name">Slätten</h2>
+					<ul class="floor_details">
+						<li>Suelo Laminado</li>
+						<li>Ancho: 19 cm</li>
+						<li>Longitud: 130 cm</li>
+						<li>Peso: 15 kg</li>
+					</ul>
+				</div>
+				<div class="floor_text_price_pic">
+					<p class="floor_price_m2">99.000 Gs/m<sup>2</sup></p>
+					<img src="floor_slatten.png" alt="Suelo slatten">
+				</div>
+			</div>
+			<div class="floor_text_calc">
+				<form>Calcule el precio total aquí<br>
+					<input class="floor_input_spinner" type="number" placeholder="Por ejemplo 25 m2"  min="0"><br>
+					<input class="floor_input_submit" type="submit" value="Calcular">
+				</form>
+				<p class="floor_precio_total"> Tu precio total sería <br> 1.485.000 Gs</p>
+			</div>
+		</div>
+		<div class="project_foto_area">
+			<img src="before_slatten.png" alt="Antes Slatten">
+			<img src="after_slatten.png" alt="Después Slatten">
+			<br>
+			<a href="">Antes</a>
+			<p>|</p>
+			<a href="">Después</a>
+		</div>
 	</div>
-	<div class="before_after_pic">
-		<img src="before_slatten.png" alt="Antes Slatten">
-		<img src="after_slatten.png" alt="Después Slatten">
-		<br>
-		<a href="">Antes</a>
-		<p>|</p>
-		<a href="">Después</a>
-	</div>
-</div>
 
-<div class="floor_special">
-	<div class="before_after_pic">
-		<img src="before_tundra.png" alt="Antes Tundra">
-		<img src="after_tundra.png" alt="Después Tundra">
-		<br>
-		<a href="">Antes</a>
-		<p>|</p>
-		<a href="">Después</a>
-	</div>
-	<div class="floor_calc">
-		<h2 class="floor_name">Tundra</h2>
-		<p class="floor_price_m2"> 129.000 Gs/m<sup>2</sup> </p>
-		<ul>
-			<li>Suelo Laminado</li>
-			<li>Ancho: 19 cm</li>
-			<li>Longitud: 130 cm</li>
-			<li>Peso: 15 kg</li>
-		</ul>
-		<img src="floor_tundra.png" alt="Suelo Tundra">
-		<form>
-	  		Calcule el precio aquí<br>
-	  		<input type="number" placeholder="p. ej. 25 metros cuadrados" min="0"><br>
-	  		<input type="submit" value="Calcular">
-		</form>
-		<p class="precio_total"> Tu precio total sería <br> 1.485.000 Gs</p>
+	<div class="project_segment cf">
+		<div class="project_foto_area opposite_segment">
+			<img src="before_tundra.png" alt="Antes Tundra">
+			<img src="after_tundra.png" alt="Después Tundra">
+			<br>
+			<a href="">Antes</a>
+			<p>|</p>
+			<a href="">Después</a>
+		</div>
+		<div class="project_text_area floor_calc opposite_segment">
+			<h2 class="floor_name">Tundra</h2>
+			<p class="floor_price_m2"> 129.000 Gs/m<sup>2</sup></p>
+			<ul class="floor_details">
+				<li>Suelo Laminado</li>
+				<li>Ancho: 19 cm</li>
+				<li>Longitud: 130 cm</li>
+				<li>Peso: 15 kg</li>
+			</ul>
+			<img src="floor_tundra.png" alt="Suelo Tundra">
+			<form>Calcule el precio aquí<br>
+				<input type="number" placeholder="p. ej. 25 metros cuadrados" min="0"><br>
+				<input type="submit" value="Calcular">
+			</form>
+			<p class="precio_total"> Tu precio total sería <br> 1.485.000 Gs</p>
+		</div>
 	</div>
 </div>
