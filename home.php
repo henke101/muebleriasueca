9ce0c12bb8d8763491d1c4c33ef4e359822b3ae8
Henrik Andersson <heenrik@hotmail.com>
2016-02-25 20:59:42 -0300

    add style to featured products home page

--- home.php
+++ home.php
@@ -5,8 +5,8 @@
 	</p>
 	<a href="?page=floors">View the floor special</a>
 	<form>
-  		<input type="number" placeholder="Calcule el precio aquí, p. ej. 25 m2"  min="0"><br>
-  		<input type="submit" value="Calcular">
+		<input type="number" placeholder="Calcule el precio aquí, p. ej. 25 m2"  min="0"><br>
+		<input type="submit" value="Calcular">
 	</form>
 	<p class="startpage_precio_total"> Tu precio total sería <br> 1.485.000 Gs
 	</p>
@@ -19,48 +19,59 @@
 	<a class="arrow arrow-prev" href="">Previous</a>
 </div>
 <div>
-	<article>
-		<img src="http://placehold.it/300x400">
-		<h1>MALM</h1>
-		<p>Cama</p>
-		<p>3.000.000 Gs</p>
-		<p><s>3.500.000 Gs</s></p>
+	<article class="prod_thumb">
+		<div class="prod_thumb_text">
+			<h1>MALM</h1>
+			<p class="prod_thumb_descr">Cama</p>
+			<p class="prod_thumb_price">3.000.000 Gs</p>
+		</div>
+		<a href="?page=product">
+			<img src="http://placehold.it/286x382">
+		</a>
 	</article>
-	<article>
-		<img src="http://placehold.it/300x400">
-		<h1>MALM</h1>
-		<p>Cama</p>
-		<p>3.000.000 Gs</p>
-		<p><s>3.500.000 Gs</s></p>
+	<article class="prod_thumb">
+		<div class="prod_thumb_text">
+			<h1>MALM</h1>
+			<p class="prod_thumb_descr">Cama</p>
+			<p class="prod_thumb_price">3.000.000 Gs</p>
+		</div>
+		<a href="?page=product">
+			<img src="http://placehold.it/286x382">
+		</a>
 	</article>
-	<article>
-		<img src="http://placehold.it/300x400">
-		<h1>MALM</h1>
-		<p>Cama</p>
-		<p>3.000.000 Gs</p>
-		<p><s>3.500.000 Gs</s></p>
+	<article class="prod_thumb">
+		<div class="prod_thumb_text">
+			<h1>MALM</h1>
+			<p class="prod_thumb_descr">Cama</p>
+			<p class="prod_thumb_price">3.000.000 Gs</p>
+		</div>
+		<a href="?page=product">
+			<img src="http://placehold.it/286x382">
+		</a>
 	</article>
-	<article>
-		<img src="http://placehold.it/300x400">
-		<h1>MALM</h1>
-		<p>Cama</p>
-		<p>3.000.000 Gs</p>
-		<p><s>3.500.000 Gs</s></p>
+	<article class="prod_thumb">
+		<div class="prod_thumb_text">
+			<h1>MALM</h1>
+			<p class="prod_thumb_descr">Cama</p>
+			<p class="prod_thumb_price">3.000.000 Gs</p>
+		</div>
+		<a href="?page=product">
+			<img src="http://placehold.it/286x382">
+		</a>
 	</article>
-</div>
-<div>
-	<p>
-		“That Muebleria Sueca shit, it was aight, it was aighty ouknowI'msayin, that shit was aight, it was cool. But my shit is J--more John Blaze than that! I got John Blaze shit” <br>
-		- The Madd Rapper, N.Y, USA
-	</p>
-	<p>
-		“Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut posuere consectetur massa. Vivamus accumsan convallis eleifend. Praesent imperdiet placerat nibh et posuere.” <br>
-		- Latin guy, Rome, Italy
-	</p>
-	<ul>
-		<li><a href="">Testimonio1</a></li>
-		<li><a href="">Testimonio2</a></li>
-		<li><a href="">Testimonio3</a></li>
-		<li><a href="">Testimonio4</a></li>
-	</ul>
-</div>
+	<div>
+		<p>
+			“That Muebleria Sueca shit, it was aight, it was aighty ouknowI'msayin, that shit was aight, it was cool. But my shit is J--more John Blaze than that! I got John Blaze shit” <br>
+			- The Madd Rapper, N.Y, USA
+		</p>
+		<p>
+			“Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut posuere consectetur massa. Vivamus accumsan convallis eleifend. Praesent imperdiet placerat nibh et posuere.” <br>
+			- Latin guy, Rome, Italy
+		</p>
+		<ul>
+			<li><a href="">Testimonio1</a></li>
+			<li><a href="">Testimonio2</a></li>
+			<li><a href="">Testimonio3</a></li>
+			<li><a href="">Testimonio4</a></li>
+		</ul>
+	</div>
